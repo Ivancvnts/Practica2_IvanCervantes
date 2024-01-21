9a2c4ab7b48edabbd177a68a3e986d470ea756bd
Ivancvnts <ivancvnts@hotmail.com>
2024-01-21 16:33:38 +0100

    Commit 4

--- Practica2_IvanCervantes/Practica2_IvanCervantes/Practica2_IvanCervantes/Practica2_IvanCervantes.cpp
+++ Practica2_IvanCervantes/Practica2_IvanCervantes/Practica2_IvanCervantes/Practica2_IvanCervantes.cpp
@@ -8,6 +8,7 @@ int main()
     std::cout << "Commit 1\n";
     std::cout << "Commit 2\n";
     std::cout << "Conflicto 1\n";
-    std::cout << "Commit 3\n";
+    std::cout << "Commit 33\n";
+    std::cout << "Commit 4\n";
 
 }
